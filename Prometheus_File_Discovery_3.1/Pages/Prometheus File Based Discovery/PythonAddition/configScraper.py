5f8070c9ff926c423e10ef315bc9f12449988417
Laeubin, Joel <joel.laeubin@juliusbaer.com>
2022-03-03 12:25:08 +0100

    Added Python configScraper.py script

---
+++ Prometheus_File_Discovery_3.1/Pages/Prometheus File Based Discovery/PythonAddition/configScraper.py
@@ -0,0 +1,105 @@
+'''
+Python:         Python 2.7.16 (v2.7.16:413a49145e, Mar  4 2019, 01:37:19) [MSC v.1500 64 bit (AMD64)]
+Context:        This script belongs to the Prometheus Configurator.
+Author:         Joel Laeubin
+Dependencies:   urllib3[secure] because of TLS warnings, check https://urllib3.readthedocs.io/en/1.26.x/user-guide.html#ssl
+'''
+
+# imports
+import logging
+import sys
+import requests
+import json
+import hashlib
+import logging
+import os
+import shutil
+
+# --------------------------------------------------------------------------------------------- Setup
+# configure logging
+logging.basicConfig(filename='configScraper.log', level=logging.WARNING,
+                    format='%(asctime)s|%(levelname)s|%(message)s', datefmt='%d/%m/%Y-%I:%M:%S-%p')
+
+# delete logging file if size > 5MB
+try:
+    if os.stat(sys.path[0] + '\\' + 'configScraper.log').st_size > 5000000:
+        os.remove(sys.path[0] + '\\' + 'configScraper.log')
+except IOError as e:
+    logging.warning('Logfile-Remove-IO-Exception: {}'.format(e.strerror))
+
+# placeholders for global scope
+local_md5 = str()
+api_md5 = str()
+md5_request = None
+config_request = None
+
+# set default config paths for directories & create dirs if required
+local_base_path = str(sys.path[0] + '\\current')
+local_config_path = str(local_base_path + '\\prometheus.yml')  # Default prometheus config path
+local_backup_path = str(sys.path[0] + '\\backup')
+# Create backup folder if required
+try:
+    if not os.path.isdir(local_base_path):
+        os.mkdir(local_base_path)
+    if not os.path.isdir(local_backup_path):
+        os.mkdir(local_backup_path)
+except IOError as e:
+    logging.warning('DirSetup-IO-Exception: {}'.format(e.strerror))
+
+# catch varargs & construct urls
+base_url = str(sys.argv[1])
+md5_endpoint = base_url + "/getChecksum"
+config_endpoint = base_url + "/getConfig"
+
+# checking for optional 2nd vararg containing filepath of local config
+if len(sys.argv) > 2:
+    local_config_path = str(sys.argv[2])
+    local_base_path = str(sys.argv[2]).replace('\\prometheus.yml', '')
+
+# --------------------------------------------------------------------------------------------- MD5 comparison
+# grab md5-checksum of the main prometheus.yml file on this server & compare it with the one hosted
+try:
+    with open(local_config_path, 'rb') as file:
+        local_md5 = hashlib.md5(file.read()).hexdigest()
+except IOError as e:
+    logging.warning('FileRead-IO-Exception: {}'.format(e.strerror))
+
+# get md5-checksum from api
+try:
+    md5_request = requests.get(md5_endpoint, verify=False)
+except requests.exceptions.RequestException as e:
+    logging.warning('Requests-Exception: {}'.format(e.strerror))
+if md5_request is not None:
+    api_md5 = json.loads(md5_request.text)
+    api_md5 = api_md5['Checksum']
+
+# --------------------------------------------------------------------------------------------- Backup & config pull
+# only proceed when config changed, hence local_md5 != api_md5
+if local_md5 != api_md5:
+
+    # Iterate over each file (skips directories) in saved location
+    try:
+        for local_file in [f for f in os.listdir(local_base_path) if os.path.isfile(os.path.join(local_base_path, f))]:
+            if '.json' in local_file or '.yml' in local_file:
+                shutil.copyfile(local_base_path + '\\' + local_file, local_backup_path + '\\' + local_file)
+    except IOError as e:
+        logging.warning('Backup-IO-Exception: {}'.format(e.strerror))
+
+    # make request, we may need to specify a private certificate using verify='pathToCertificate'
+    try:
+        config_request = requests.get(config_endpoint, verify=False)
+    except requests.exceptions.RequestException as e:
+        logging.warning('Requests-Exception: {}'.format(e.strerror))
+
+    if config_request is not None:
+        response = config_request.text
+
+        # Convert to JSON object to generate files from it
+        json_object = json.loads(response)
+        try:
+            for entry in json_object.items():
+                with open(local_base_path + '\\' + entry[0], 'w') as config_file:
+                    content = str(entry[1]).replace('\r', "")
+                    config_file.write(content)
+        except IOError as e:
+            logging.warning('FileWrite-IO-Exception: {}'.format(e.strerror))
