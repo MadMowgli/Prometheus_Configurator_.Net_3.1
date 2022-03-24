08498057ac8c986db6400f445bc97b4b33ed23d7
275e2153-0fda-456b-9683-4c98048c0037 <joel.laeubin@juliusbaer.com>
2022-03-24 15:14:41 +0100

    Added enhanced graphical feedback

---
+++ Prometheus_File_Discovery_3.1/wwwroot/js/showToast.js
@@ -0,0 +1,11 @@
+﻿async function showToast() {
+    const toast = document.getElementById('toastEr');
+    const bootstrapToast = new bootstrap.Toast(toast);
+    bootstrapToast.show();
+    await this.sleep(5000);
+    bootstrapToast.hide();
+}
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
